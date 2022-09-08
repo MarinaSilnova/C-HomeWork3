7a26007c7da6827da92a2a5a46cfc9b5585b5f3a
Marina Silnova <marusia421@mail.ru>
2022-09-08 16:19:03 +0300

    add ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(Convert.ToString(n)[0] == Convert.ToString(n)[4] && Convert.ToString(n)[1] == Convert.ToString(n)[3])
+    Console.WriteLine(" yes");
+else
+    Console.WriteLine(" no");
+
