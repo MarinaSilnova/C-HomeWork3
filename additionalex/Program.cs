37645ea04cb17a48e72b739e63c553151cb93328
Marina Silnova <marusia421@mail.ru>
2022-09-09 08:20:38 +0300

    Добавили решение доп задачи

---
+++ additionalex/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите количество дней ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int i = 0;
+int m = 1;
+while(i < n)
+{
+    int e = Convert.ToInt32(Console.ReadLine());
+    if(e > 0)
+    {
+        count = count + 1;
+        if (m < count)
+        m = count;
+    }
+    else
+    count = 0;
+    i++;
+}
+Console.WriteLine(m);
